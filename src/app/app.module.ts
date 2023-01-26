d3923d7965fd7f2207162344c593ad077efb09d1
mct <59967078+mauroctz@users.noreply.github.com>
2023-01-26 11:57:06 +0100

    creazione di: -pagine: Home - Users - ModalUser -componenti: cardUser - pagination -interfaccie: User

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
+import { HttpClientModule } from "@angular/common/http";
+import { MyApp } from "./app.component";
+import { CommonModule } from "@angular/common";
+import { HomePage } from "../pages/home/home";
+import { UsersPage } from "./../pages/users/users";
+import { ModalUserPage } from "./../pages/modal-user/modal-user";
+import { PaginationComponent } from "./../components/pagination/pagination";
+import { CardUserComponent } from "./../components/card-user/card-user";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    UsersPage,
+    ModalUserPage,
+    CardUserComponent,
+    PaginationComponent,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp, {
+      iconMode: "ios",
+      modalEnter: "modal-slide-in",
+      modalLeave: "modal-slide-out",
+      tabsPlacement: "bottom",
+      pageTransition: "ios-transition",
+    }),
+    HttpClientModule,
+    CommonModule,
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage, ModalUserPage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+  ],
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,81 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { NavController } from "ionic-angular";
+import { User } from "../../interfaces/User";
+import { ModalController, NavParams } from "ionic-angular";
+import { ModalUserPage } from "../modal-user/modal-user";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html",
 })
 export class HomePage {
+  title: String = "Home";
+  users: Array<User> = [];
+  totalPages: number;
+  currentPage: number = 1;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public http: HttpClient,
+    public modalCtrl: ModalController
+  ) {}
 
+  ngOnInit(): void {
+    this.getUsers();
   }
 
+  async getUsers(page: number = 1) {
+    try {
+      const response = await this.http
+        .get(`https://reqres.in/api/users?page=${page}`)
+        .toPromise()
+        .then((data) => data);
+      this.users = response["data"];
+      this.totalPages = response["total_pages"];
+      this.currentPage = page;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.getUsers(this.currentPage + 1);
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.getUsers(this.currentPage - 1);
+    }
+  }
+
+  isFirstPage(): boolean {
+    return this.currentPage === 1;
+  }
+
+  isLastPage(): boolean {
+    return this.currentPage === this.totalPages;
+  }
+
+  openModal(user: User): void {
+    const modal = this.modalCtrl.create(ModalUserPage, { ...user });
+    modal.present();
+
+    modal.onDidDismiss((user) => {
+      if (user) {
+        this.users = this.users.map((u) => {
+          if (u.id === user.id) {
+            return {
+              ...u,
+              first_name: user.first_name,
+              last_name: user.last_name,
+            };
+          }
+          return { ...u };
+        });
+      }
+    });
+  }
 }
