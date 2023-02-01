e09f1bf00b29278f375f2352cb1ce2df7b5c995b
mct <59967078+mauroctz@users.noreply.github.com>
2023-02-01 08:54:05 +0100

    deat: esempio di wahtcher usando get e set...-chiama una funzione al setter

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -16,11 +16,24 @@ import { User } from "../../interfaces/Api";
 })
 export class UsersPage {
   title: String = "All users";
-  users: Array<User> = [];
+  _users: Array<User> = [];
   constructor(public navCtrl: NavController, public http: HttpClient) {}
 
   ngOnInit() {
     this.getAllUsers();
+  }
+
+  get users() {
+    return this._users;
+  }
+
+  set users(_users: Array<User>) {
+    this._users = _users;
+    this.usersChanges();
+  }
+
+  usersChanges() {
+    console.log(this.users);
   }
 
   async getAllUsers(page: number = 1) {
