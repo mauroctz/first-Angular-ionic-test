1f43afb509ad96c0ec62d43b54deb8797905a7a2
mct <59967078+mauroctz@users.noreply.github.com>
2023-02-03 16:01:26 +0100

    feat: refactor and fix some bug when normalize  data

--- src/components/donut-chart/donut-chart.pipe.ts
+++ src/components/donut-chart/donut-chart.pipe.ts
@@ -12,24 +12,25 @@ interface DonutSliceWithCommands extends DonutSlice {
 export class DonutChartPipe implements PipeTransform {
   transform(
     donutSlices: DonutSlice[],
-    radius: number,
+    svgRadius: number,
     svgSize: number,
     borderSize: number,
     borderRounded: boolean,
-    size: string,
+    chartType: string,
     sliceSelected: DonutSlice = null
   ): DonutSliceWithCommands[] {
     let previousPercent = 0;
+
     return donutSlices.map((slice) => {
       const sliceWithCommands: DonutSliceWithCommands = {
         ...slice,
         commands: this.getSliceCommands(
           slice,
-          radius,
+          svgRadius,
           svgSize,
           borderSize,
           borderRounded,
-          size,
+          chartType,
           sliceSelected
         ),
         offset: previousPercent * 3.6 * -1,
@@ -38,31 +39,32 @@ export class DonutChartPipe implements PipeTransform {
       return sliceWithCommands;
     });
   }
+  getFullPercentageValueSliceCommand() {}
 
   getSliceCommands(
     donutSlice: DonutSlice,
-    radius: number,
+    svgRadius: number,
     svgSize: number,
     borderSize: number,
     borderRounded: boolean,
-    size: string,
+    chartType: string,
     sliceSelected: DonutSlice
   ): string {
-    const degrees = this.percentToDegrees(donutSlice.percent, size);
-    const longPathFlag = degrees > (size === "half" ? 90 : 180) ? 1 : 0;
+    const degrees = this.percentToDegrees(donutSlice.percent, chartType);
+    const longPathFlag = degrees > (chartType === "half" ? 90 : 180) ? 1 : 0;
     const innerRadius =
-      radius -
+      svgRadius -
       borderSize +
       (sliceSelected && donutSlice.id === sliceSelected.id ? -2 : 0);
 
     const commands: string[] = [];
-    commands.push(`M ${svgSize / 2 + radius} ${svgSize / 2}`);
+    commands.push(`M ${svgSize / 2 + svgRadius} ${svgSize / 2}`);
     commands.push(
-      `A ${radius} ${radius} 0 ${longPathFlag} 0 ${this.getCoordFromDegrees(
+      `A ${svgRadius} ${svgRadius} 0 ${longPathFlag} 0 ${this.getCoordFromDegrees(
         degrees,
-        radius,
+        svgRadius,
         svgSize,
-        size
+        chartType
       )}`
     );
     commands.push(
@@ -70,11 +72,11 @@ export class DonutChartPipe implements PipeTransform {
         degrees,
         innerRadius,
         svgSize,
-        size
+        chartType
       )}`
     );
     commands.push(
-      `L ${this.getCoordFromDegrees(degrees, innerRadius, svgSize, size)}`
+      `L ${this.getCoordFromDegrees(degrees, innerRadius, svgSize, chartType)}`
     );
     commands.push(
       `A ${innerRadius} ${innerRadius} 0 ${longPathFlag} 1 ${
@@ -82,24 +84,24 @@ export class DonutChartPipe implements PipeTransform {
       } ${svgSize / 2}`
     );
     borderRounded &&
-      commands.push(`A 1 1 0 0 ${size === "half" ? 0 : 1} 100 50`);
+      commands.push(`A 1 1 0 0 ${chartType === "half" ? 0 : 1} 100 50`);
     return commands.join(" ");
   }
 
   getCoordFromDegrees(
     angle: number,
-    radius: number,
+    svgRadius: number,
     svgSize: number,
-    size: string
+    chartType: string
   ): string {
-    const x = Math.cos((angle * Math.PI) / (size === "half" ? 90 : 180));
-    const y = Math.sin((angle * Math.PI) / (size === "half" ? 90 : 180));
-    const coordX = x * radius + svgSize / 2;
-    const coordY = y * -radius + svgSize / 2;
+    const x = Math.cos((angle * Math.PI) / (chartType === "half" ? 90 : 180));
+    const y = Math.sin((angle * Math.PI) / (chartType === "half" ? 90 : 180));
+    const coordX = x * svgRadius + svgSize / 2;
+    const coordY = y * -svgRadius + svgSize / 2;
     return [coordX, coordY].join(" ");
   }
 
-  percentToDegrees(percent: number, size: string): number {
-    return (percent = size == "half" ? percent * 1.8 : percent * 3.6);
+  percentToDegrees(percent: number, chartType: string): number {
+    return (percent = chartType == "half" ? percent * 1.8 : percent * 3.6);
   }
 }
--- src/components/donut-chart/donut-chart.ts
+++ src/components/donut-chart/donut-chart.ts
@@ -12,24 +12,76 @@ import {
   templateUrl: "donut-chart.html",
 })
 export class DonutChartComponent implements OnInit {
-  @Input() radius = 50;
-  @Input() height = 100;
   @Input() borderSize = 20;
   @Input() borderRounded: boolean = true;
   @Input() data: DonutSlice[] = [];
-  @Input() sort?: "asc" | "desc"; //asc or desc
-  @Input() size?: "full" | "half";
+  @Input() sort?: "asc" | "desc" = null;
+  @Input() chartType?: "full" | "half";
   @Input() customTotal: number = null;
-
-  sliceSelected: DonutSlice = null;
+  private svgRadius: number = 50;
+  private svgSize: number = 100;
+  private sliceSelected: DonutSlice = null;
+  private dimensionChart: number = 100;
+  private sumPercentage: number = 0;
 
   ngOnInit() {
-    this.sortItems(this.sort);
+    if (this.sort != null) this.data = [...this.sortItems(this.sort)];
+
+    this.getDimensionChart();
     this.calculatePercentages();
   }
 
-  sortItems(sort) {
-    this.data.sort((a, b) => {
+  calculatePercentages(typeValue: string = "value"): void {
+    let sum = this.data.reduce((acc, slice) => acc + slice[typeValue], 0);
+    sum = this.customTotal > sum ? this.customTotal : sum;
+    sum = this.chartType === "half" ? sum * 2 : sum;
+    this.data = [
+      ...this.data.map((slice) => {
+        let percent = Math.round((slice[typeValue] / sum) * 100);
+
+        if (percent === 0 && slice["value"] > 0) {
+          percent = 1;
+        }
+
+        console.log(`percentage ${percent}`);
+        return { ...slice, percent: percent };
+      }),
+    ];
+    if (!this.customTotal) {
+      this.sumPercentage = this.getTotalPercentage();
+      if (this.sumPercentage > this.dimensionChart) {
+        this.normalize(typeValue);
+      }
+    }
+  }
+
+  normalize(typeValue: string = "value"): void {
+    console.log(`*****normalize***** `);
+    if (typeValue == "percent") {
+      console.log("before : ", this.data);
+
+      const orderedArray: DonutSlice[] = this.sortItems("desc");
+      console.log("after : ", this.data);
+      for (let i = 0; i < this.sumPercentage - this.dimensionChart; i++) {
+        this.data = this.data.map((item) => {
+          if (item.id === orderedArray[i].id) {
+            console.log({ ...item });
+            return { ...item, percent: item.percent - 1 };
+          } else {
+            return { ...item };
+          }
+        });
+      }
+      console.log(this.data);
+
+      return;
+    }
+    this.calculatePercentages("percent");
+  }
+
+  sortItems(sort: String): DonutSlice[] {
+    let array: DonutSlice[] = [...this.data];
+    return array.sort((a, b) => {
       if (sort === "asc") {
         return a.value < b.value ? -1 : a.value > b.value ? 1 : 0;
       } else if (sort === "desc") {
@@ -38,35 +90,13 @@ export class DonutChartComponent implements OnInit {
     });
   }
 
-  calculatePercentages(type: string = "value"): void {
-    let sum = this.data.reduce((acc, slice) => acc + slice[type], 0);
-    sum = this.customTotal > sum ? this.customTotal : sum;
-    sum = this.size === "half" ? sum * 2 : sum;
-    this.data = [
-      ...this.data.map((slice) => {
-        let percent = Math.round((slice[type] / sum) * 100);
-        if (percent === 0 && slice["value"] > 0) {
-          percent = 1;
-        }
-        console.log(`percentage ${percent}`);
-
-        return { ...slice, percent: percent };
-      }),
-    ];
-    if (!this.customTotal) {
-      const sumPercentage = this.data.reduce(
-        (acc, slice) => acc + slice["percent"],
-        0
-      );
-      console.log(
-        `total % = ${sumPercentage}/${this.size === "half" ? 50 : 100}`
-      );
-      if (sumPercentage !== (this.size === "half" ? 50 : 100)) {
-        console.log(`*****normalize***** `);
-
-        this.calculatePercentages("percent");
-      }
-    }
+  getTotalPercentage(): number {
+    let total = this.data.reduce((acc, slice) => acc + slice["percent"], 0);
+    console.log(`total ${total}`);
+    return total;
+  }
+  getDimensionChart(): void {
+    this.dimensionChart = this.chartType === "half" ? 50 : 100;
   }
 
   selectSlice(slice: DonutSlice) {
